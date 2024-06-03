4d3b97c73298f4f910ca78948cbc8a740b1eec75
Pratyush Birole <birole.pratyush@gmail.com>
2024-06-03 23:12:19 +0530

    Fixing App Routing useNavigate --> wrapping up components

--- src/App.js
+++ src/App.js
@@ -1,22 +1,30 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 import Navbar from './Components/Navbar';
 import Header from './Components/Header';
 import Reviews from './Components/Reviews';
 import Download from './Components/Download';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Policies from './Components/Policies'; // Make sure to create this component
 
 function App() {
   return (
-    <div className="App">
-    <Navbar/>
-    <Header/>
-    <br/>
-    <Reviews/>
-    <br/>
-    <br/>
-
-    <Download/>
-    </div>
+    <Router>
+      <div className="App">
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Header />} />
+          <Route path="/reviews" element={<Reviews />} />
+          <Route path="/download" element={<Download />} />
+          <Route path="/policies" element={<Policies />} />
+        </Routes>
+        <br />
+        <Reviews />
+        <br />
+        <br />
+        <Download />
+      </div>
+    </Router>
   );
 }
 
